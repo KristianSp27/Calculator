79269a5a55e9dd3531a25c3d6794a887caf12fec
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2023-02-27 00:04:39 +0200

    Update

--- script.js
+++ script.js
@@ -75,6 +75,8 @@ class Calculator {
     this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand);
     if (this.operation != null) {
       this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = "";
     }
   }
 }
