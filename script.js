5dab846bbede1ca3f09ae96d5aeb853731dd61c3
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2023-02-27 00:03:39 +0200

    Update

--- script.js
+++ script.js
@@ -53,9 +53,29 @@ class Calculator {
     this.operation = undefined;
     this.previousOperand = "";
   }
+
+  getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[0];
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("en", { maximumFractionDigits: 0 });
+    }
+    if (decimalDigits !== null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
+  }
+
   updateDisplay() {
-    this.currentOperandTextElement.innerText = this.currentOperand;
-    this.previousOperandTextElement.innerText = this.previousOperand;
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand);
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`;
+    }
   }
 }
 
